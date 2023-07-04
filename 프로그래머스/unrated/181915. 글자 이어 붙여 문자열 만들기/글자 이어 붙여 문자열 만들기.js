5664d8a825083f5278055ebd5fe22e59d44c6182
Ella Lim <122195011+trtw9889@users.noreply.github.com>
2023-07-04 11:18:46 +0900

    [unrated] Title: 글자 이어 붙여 문자열 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -0,0 +1,8 @@
+const solution = (my_string, index_list) => {
+    let result = ""
+    
+    for (let i=0; i < index_list.length; i++){
+        result += my_string[index_list[i]]
+    }
+    return result
+}
