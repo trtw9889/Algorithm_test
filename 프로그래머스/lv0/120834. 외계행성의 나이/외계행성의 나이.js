6a5e1333bac6b51a39b47f1f5e1532f5cc8a3639
Ella Lim <122195011+trtw9889@users.noreply.github.com>
2023-10-05 11:56:40 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,19 @@
+function solution(age) {
+    const str = {
+        0: "a",
+        1: "b",
+        2: "c",
+        3: "d",
+        4: "e",
+        5: "f",
+        6: "g",
+        7: "h",
+        8: "i",
+        9: "j"
+    }
+    let arr = []
+    const result = String(age).split("")
+    result.map(x => arr.push(str[x]))
+    
+    return arr.join("")
+}
