5af12db7156cd8461071d4d4b49aea2c37cb0d48
Ella Lim <122195011+trtw9889@users.noreply.github.com>
2023-12-08 17:51:02 +0900

    [level unrated] Title: 세로 읽기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
@@ -0,0 +1,10 @@
+function solution(my_string, m, c) {
+    const arr = my_string.split("")
+    let result = []
+    
+    for(let i = 0; i < arr.length ; i += m ) {
+        arr.slice(i,i+m)
+        result.push(arr[i+c-1])
+    }
+    return result.join("")
+}
