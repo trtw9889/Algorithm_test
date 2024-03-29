5870ddabbfa51ed2d45a96296f472ead826be05d
Ella Lim <122195011+trtw9889@users.noreply.github.com>
2024-03-29 11:18:03 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,25 @@
+function solution(date1, date2) {
+    const [year1,month1,day1] = date1
+    const [year2,month2,day2] = date2
+    
+    if(year1 < year2) {
+        return 1
+    } else if(year1 > year2) {
+        return 0
+    }
+    
+    if(month1 < month2) {
+        return 1
+    } else if(month1 > month2) {
+        return 0
+    }
+    
+    if(day1 < day2) {
+        return 1
+    } else if(day1 > day2) {
+        return 0
+    }
+    
+    return 0
+    
+}
