0bbea0775e07b7081c2cfcbdd02f9d3c9669e1e8
Ella Lim <122195011+trtw9889@users.noreply.github.com>
2024-04-04 09:05:32 +0900

    [level 1] Title: K번째수, Time: 4.51 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,13 @@
+function solution(array, commands) {
+    
+    const num = commands.map((val)  => {
+        const result = array.slice(val[0] - 1, val[1]).sort((a,b) => a-b)
+        console.log(result)
+        const numVal = val[2]
+        
+        return result[numVal-1]
+    })
+     return num
+}
+
+
